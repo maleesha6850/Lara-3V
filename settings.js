3089b0421a41b0c142f5979ac7029249a9396b6f
maleesha6850 <maleeshamp942@gmail.com>
2026-01-01 19:14:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "iRow1TAB#ZPBL2JrSZ4ZEQzZRYmw06BM8jc6yKiNeq9ZIUGS-XL0", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
